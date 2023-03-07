b55578462c4bdac1750be9f67595e99d422ea625
Hetvi Vora <hetvivora3007@gmail.com>
2023-03-07 23:19:20 +0530

    added some css

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import "./Navbar.css"
 
@@ -9,11 +9,11 @@ const Navbar = () => {
             <div className='navbar-container'>
                 <div className='navbar-content'>
                     <div className='navbar-items'>
-                        <Link className='link' to="/">Home</Link>
-                        <Link className='link' to="/shopall">Shop All</Link>
-                        <Link className='link' to="/ourstory">Our Story</Link>
-                        <Link className='link' to="/ourcraft">Our Craft</Link>
-                        <Link className='link' to="/contact">Contact</Link>
+                        <NavLink className='link' to="/">Home</NavLink>
+                        <NavLink className='link' to="/shopall">Shop All</NavLink>
+                        <NavLink className='link' to="/ourstory">Our Story</NavLink>
+                        <NavLink className='link' to="/ourcraft">Our Craft</NavLink>
+                        <NavLink className='link' to="/contact">Contact</NavLink>
                     </div>
                 </div>
             </div>
